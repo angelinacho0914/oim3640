3df6462494eb6adc326b9a8daa4d57c5b0e6d71d
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-01-22 12:21:53 -0500

    Exercise 2

---
+++ session02/calc.py
@@ -0,0 +1,17 @@
+# ex02-1
+a = 'There are '
+x = 42*60+42
+b = ' seconds in 42 minutes 42 seconds.'
+print(a + str(x) + b)
+
+# ex02-2
+c = 'There are '
+y = 10/1.61
+d = ' miles in 10 kilometers.'
+print(c + str(y) + d)
+
+# ex02-3
+e = 'The average speed is '
+z = y/0.78
+f = ' miles per hour.'
+print(e + str(z) + f)
