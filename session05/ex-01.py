6de3472ccd950ef26a0bd1644c6304965ea64c6a
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-02-01 19:31:13 -0500

    exercise 1 of session 05

---
+++ session05/ex-01.py
@@ -0,0 +1,24 @@
+import math
+
+ratio = 100
+print(math.log10(ratio))
+
+degrees = 45
+radians = degrees / 180.0 * math.pi
+print(math.sin(radians))
+
+# Round the number to however much digit wanted
+num1 = 4.5
+print(round(num1, 0))
+
+# Find the smallest number
+print(min(41, 33, 12))
+
+# Given a string return an integer representing the Unicode character
+print(ord("b"))
+
+# Return the string representing a character whose Unicode integer points
+print(chr(8364))
+
+# Return C in alphabet
+print(chr(ord("A") + 2))
