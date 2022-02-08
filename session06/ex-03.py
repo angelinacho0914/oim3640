458a713ae2cdf91291000ff1b849868209a21e1c
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-02-08 12:24:22 -0500

    added docstring

--- session06/ex-03.py
+++ session06/ex-03.py
@@ -7,6 +7,7 @@ print(angie)
 
 # Exercise 3.1
 def shape1(t, n, angle):
+    """Takes in 3 parameters to construct a shape by drawing a lot of squares."""
     for i in range(n):
         for j in range(4):
             t.fd(100)
@@ -19,6 +20,7 @@ def shape1(t, n, angle):
 
 # Exercise 3.2
 def shape2(t, n, angle, length):
+    """Takes in 4 parameters to construct a spiral by drawing a lot of squares, each larger than the previous, in different angles."""
     for i in range(n):
         for j in range(4):
             t.fd(length)
@@ -32,6 +34,7 @@ def shape2(t, n, angle, length):
 
 # Exercise 3.3
 def star(t, n, length):
+    """Takes in 3 parameters and constructs a spiral by drawing stars, each bigger than the previous, and by accumulating angles."""
     angle = 180 - 36  # angle opposite of 36
 
     t.lt(36)
@@ -48,6 +51,7 @@ def star(t, n, length):
 
 # Exercise 3.4
 def shape4(t, r):
+    """Constructing an archimedian spiral by using the circle function in turtle module."""
     for i in range(120):
         t.circle(r + i, 60)
 
