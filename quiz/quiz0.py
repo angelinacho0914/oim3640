e98f9a674d25d5f09d4a0b2943d8e969770a3ee7
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-02-08 15:53:14 -0500

    finish quiz0

--- quiz/quiz0.py
+++ quiz/quiz0.py
@@ -10,3 +10,12 @@ Notice:
 2. Your function should include docstring.
 3. Write your own test code, i.e. call the function.
 """
+
+
+def weight(n):
+    """This function ask for a parameter for a weight on earth and prints the equivalent weight on moon."""
+    # function weight
+    #   moon weight equals to earth weight * 0.165
+    #   print moon weight
+    moon = n * 0.165
+    print(f"The weight of the object in moon is {moon} .")
