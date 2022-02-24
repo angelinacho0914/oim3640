21c779b384674c6bbe0d6e2f93c031120ba87f53
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-02-24 09:43:59 -0500

    update ex05

---
+++ session09/ex05.py
@@ -0,0 +1,14 @@
+# 'A' rotated by 3 is 'D' and 'Z' rotated by 1 is 'A'
+# To rotate a word, rotate each letter by the same amount.
+    # e.g. 'cheer' rotated by 7 is 'jolly' and 'melon' rorated by -10 is 'cubed'.
+
+def rotate_word(word, n):
+    '''
+    Takes a string and an integer as parameters, and returns a
+    new string that contains the letters from the original string
+    rotated by the given amount.
+    '''
+    
+    # Use ord() which converts a character to a numeric code
+    # Use chr() which converts numeric codes to characters.
+    pass
