837207a6907e569e6b958705991a38cfe3f14f8f
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-02-24 12:33:53 -0500

    Update ex05.py

--- session09/ex05.py
+++ session09/ex05.py
@@ -8,7 +8,26 @@ def rotate_word(word, n):
     new string that contains the letters from the original string
     rotated by the given amount.
     '''
-    
-    # Use ord() which converts a character to a numeric code
-    # Use chr() which converts numeric codes to characters.
-    pass
+    # iterating through the word
+    # find out the unicode of the letter
+    # add n amount to it
+    # conver it (unicode) back to a character
+    # repeat this for all the letters in the word
+    # return the word
+    word_list = ['']
+    for letter in word:
+        x = ord(letter)     # Use ord() which converts a character to a numeric code
+        x += n
+        word_list += chr(x)  # Use chr() which converts numeric codes to characters
+    new_word = ''.join(word_list)
+    return new_word
+
+
+
+def main():
+    print(rotate_word('cheer', 7))
+    print(rotate_word('melon', -10))
+
+
+if __name__ == "__main__":
+    main()
