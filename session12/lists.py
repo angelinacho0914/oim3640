60634724e90d44877411cfa88be1a6149677324f
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-03-02 11:14:15 -0500

    update session12

--- session12/lists.py
+++ session12/lists.py
@@ -58,3 +58,108 @@ t = ['a', 'b', 'c', 'd', 'e', 'f']
 t[1:3] = ['x','y']
 print(t)
 
+# This function maps the function of the method capitalize
+def capitalize_all(t):
+    res = []
+    for s in t:
+        res.append(s.capitalize())
+    return res
+
+
+# An operation like "sum" that combines a sequence of elements into a single value is called "reduce"
+t = [1, 2, 3]
+print(sum(t))
+
+
+# An operation like "only_upper" is called a filter because it selects some of the elements
+# and filters out the others
+def only_upper(t):
+    res = []
+    for s in t:
+        if s.isupper():
+            res.append(s)
+    return res
+
+
+# Deleting elements
+t = ['a','b','c','d']
+x = t.pop(1)  # pop modifies the list and returns the element that was removed
+print(x)
+print(t)
+
+x = t.pop()   # If no index is provided, it deletes and returns the last element
+print(x)
+print(t)
+
+# Another way to remove an item or items from a list is "del"
+t = ['a','b','c','d','e']
+del t[1:3]
+print(t)
+
+# If you know the element you want to remove, but not index, use "remove"
+t = ['a','b','c']
+t.remove('b')
+print(t)
+
+
+# List and strings
+# To convert from a string to a list of characters, you can use "list":
+team = "Patriots"
+t = list(team)      # Avoid using list as variable name
+print(t)            # Avoid l because it looks like 1
+
+# Breaking a string into words
+team = "New England Patriots"
+t = team.split()
+print(t)
+
+# Optional argument called delimeter specifies which characters to use as word boundaries
+# e.g. using hyphen as a delimeter
+s = 'spam-spam-spam'
+delimteter = '-'
+t = s.split(delimteter)
+print(t)
+
+# join is the inverse of split
+t = ['New', 'England', 'Patriots']
+team = '!!'.join(t)
+print(team)
+
+
+# Objects and values
+a = 'banana'
+b = 'banana'
+
+print(a is b) # Return whether they refer to the same string
+
+a = [1, 2, 3]
+b = [1, 2, 3]   # The two lists are equivalent but not identical,not the same object
+print(a is b)
+
+a = [1, 2, 3]
+b = a
+b is a
+# A object with more than one reference has more than one name,
+# so we say that the object is aliased.
+b[0] = 'something else'
+print(a)        # They refer to the same object, so if a is changed b will also change
+
+
+# Common mistakes with lists
+t = t.sort()    # Doesn't work, it only modify the argument and return None
+
+t.append(x)
+t = t + [x]
+t += [x]
+
+t.append([x])   # INCORRECT
+t = t.append(x) # INCORRECT
+t + [x]         # INCORRECT
+t = t + x       # INCORRECT
+
+# Make copies to avoid aliasing
+t = [3, 1, 2]
+t2 = t[:]
+t2.sort()
+print(t)
+print(t2)
