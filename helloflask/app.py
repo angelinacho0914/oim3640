b862b7fa558874c864b743ecfd97903ffc888434
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-03-31 15:06:09 -0400

    hello flask update

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import temp_helper
+from temp_helper import get_current_temp
 
 
 @app.route('/weather/', methods = ['GET', 'POST'])
@@ -7,7 +7,7 @@ def show_weather():
     if request.method == 'POST':
         city_name = request.form['city']
 
-        temperature = temp_helper.get_current_temp(city_name)
+        temperature = get_current_temp(city_name)
 
         return render_template('weather-result.html', city = city_name, temperature = temperature)
     else:
