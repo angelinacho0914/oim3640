475019c488b04495b34dcb7eb7c6b2f75d5751cb
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-04-21 15:03:54 -0400

    battleship

---
+++ session22/pygame_demo.py
@@ -0,0 +1,16 @@
+from pydoc import render_doc
+from tkinter import font
+from matplotlib import image
+import pygame
+
+# Create and display the surface
+pygame.display.set_mode()
+
+# Create a surface that contains an image by using:
+image.load()
+
+# Contains text:
+font.render()
+
+# Create a surface that contains nothing at all:
+pygame.Surface
